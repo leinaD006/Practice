5572a88879dac0205439bf9262e02e7f8008b7c9
Daniel Kingery <danielkingery0@gmail.com>
2026-08-31 22:26:15 -0400

    refactor to use scoreboard data structure

---
+++ XCodeTutorials/ScoreKeeper/ScoreKeeper/Scoreboard.swift
@@ -0,0 +1,16 @@
+//
+//  Untitled.swift
+//  ScoreKeeper
+//
+//  Created by Daniel Kingery on 8/31/26.
+//
+
+import Foundation
+
+struct Scoreboard {
+    var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0)
+    ]
+}
